14886ef4747ca3534d9ed0c2c586f1e23fbc8bc5
CONOR RAFTERY - Student <G00274094@gmit.ie>
2017-09-20 10:44:52 +0100

    https://tour.golang.org/welcome/o

---
+++ Problem_Sheet_1_Task2.go
@@ -0,0 +1,16 @@
+// Author: Conor Raftery
+// Date: 2017-09-20
+
+// Adapted from: https://tour.golang.org/welcome/4
+
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+
+	fmt.Println("The time is", time.Now())
+}
